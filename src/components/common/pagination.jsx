b5a1175052759e3c2186560444d5e150d519333d
TheLuckyMojo <ethanmorales0730@gmail.com>
2023-01-31 19:53:00 -0800

    5.4 - displaying pages

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,24 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
+import _ from "lodash";
 
 const Pagination = (props) => {
+  const { itemsCount, pageSize } = props;
+
+  const pageCount = Math.ceil(itemsCount / pageSize);
+  if (pageCount === 1) return null;
+  const pages = _.range(1, pageCount + 1);
+
   return (
     <nav aria-label='Page navigation example'>
       <ul className='pagination'>
-        <li className='page-item'>
-          <a className='page-link' href='#'>
-            1
-          </a>
-        </li>
-        <li className='page-item'>
-          <a className='page-link' href='#'>
-            2
-          </a>
-        </li>
-        <li className='page-item'>
-          <a className='page-link' href='#'>
-            3
-          </a>
-        </li>
+        {pages.map((page) => (
+          <li key={page} className='page-item'>
+            <a className='page-link' href='#'>
+              {page}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
