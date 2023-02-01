9d2724c423d0ccc43cc922ded96e579ef970a6c4
TheLuckyMojo <ethanmorales0730@gmail.com>
2023-01-31 19:37:07 -0800

    5.3 - create pagination interface

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,6 +6,7 @@ import Pagination from "./common/pagination";
 class Movies extends Component {
   state = {
     movies: getMovies(),
+    pageSize: 4,
   };
 
   handleDelete = (movie) => {
@@ -19,6 +20,10 @@ class Movies extends Component {
     movies[index] = { ...movies[index] };
     movies[index].liked = !movies[index].liked;
     this.setState({ movies });
+  };
+
+  handlePageChange = (page) => {
+    console.log(page);
   };
 
   render() {
@@ -65,7 +70,11 @@ class Movies extends Component {
             ))}
           </tbody>
         </table>
-        <Pagination />
+        <Pagination
+          itemsCount={count}
+          pageSize={this.state.pageSize}
+          onPageChange={this.handlePageChange}
+        />
       </React.Fragment>
     );
   }
