b7f4ad9bb063890c288d87de1df44d120f0f46c4
TheLuckyMojo <ethanmorales0730@gmail.com>
2023-02-07 20:21:35 -0800

    Self - clean up navbar component

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -3,33 +3,31 @@ import React from "react";
 const NavBar = () => {
   return (
     <nav className='navbar navbar-expand-lg bg-body-tertiary'>
-      <div className='container-fluid'>
-        <a className='navbar-brand' href='#'>
-          Vidly
-        </a>
-        <button
-          className='navbar-toggler'
-          type='button'
-          data-bs-toggle='collapse'
-          data-bs-target='#navbarNavAltMarkup'
-          aria-controls='navbarNavAltMarkup'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-        >
-          <span className='navbar-toggler-icon'></span>
-        </button>
-        <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
-          <div className='navbar-nav'>
-            <a className='nav-link active' aria-current='page' href='#'>
-              Movies
-            </a>
-            <a className='nav-link' href='#'>
-              Customers
-            </a>
-            <a className='nav-link' href='#'>
-              Rentals
-            </a>
-          </div>
+      <a className='navbar-brand' href='#'>
+        Vidly
+      </a>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-bs-toggle='collapse'
+        data-bs-target='#navbarNavAltMarkup'
+        aria-controls='navbarNavAltMarkup'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon'></span>
+      </button>
+      <div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+        <div className='navbar-nav'>
+          <a className='nav-link active' aria-current='page' href='#'>
+            Movies
+          </a>
+          <a className='nav-link' href='#'>
+            Customers
+          </a>
+          <a className='nav-link' href='#'>
+            Rentals
+          </a>
         </div>
       </div>
     </nav>
